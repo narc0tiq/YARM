90891d4ce68129d3f3f2decf2bc75846aa2e0fd1
Octav Sandulescu <github@narc.ro>
2015-10-19 08:48:42 +0000

    Update to Factorio 0.12.11.
    Closes #19; no on_load anymore, but there's on_configuration_changed
    which does what the previous on_load was supposed to do to begin with.
    Much better API this way.
    
    Barely tested, but seems to work okay.

--- control.lua
+++ control.lua
@@ -14,45 +14,37 @@ function msg_all(message)
 end
 
 
-local loaded = false
-
-game.on_init(function()
-    if loaded then return end
-    loaded = true
-
+script.on_init(function()
     local _, err = pcall(resmon.init_globals)
     if err then msg_all({"YARM-err-generic", err}) end
 end)
 
 
-game.on_load(function()
-    if loaded then return end
-    loaded = true
-
+script.on_configuration_changed(function()
     local _, err = pcall(resmon.init_globals)
     if err then msg_all({"YARM-err-generic", err}) end
 end)
 
 
-game.on_event(defines.events.on_player_created, function(event)
+script.on_event(defines.events.on_player_created, function(event)
     local _, err = pcall(resmon.on_player_created, event)
     if err then msg_all({"YARM-err-specific", "on_player_created", err}) end
 end)
 
 
-game.on_event(defines.events.on_built_entity, function(event)
+script.on_event(defines.events.on_built_entity, function(event)
     local _, err = pcall(resmon.on_built_entity, event)
     if err then msg_all({"YARM-err-specific", "on_built_entity", err}) end
 end)
 
 
-game.on_event(defines.events.on_tick, function(event)
+script.on_event(defines.events.on_tick, function(event)
     local _, err = pcall(resmon.on_tick, event)
     if err then msg_all({"YARM-err-specific", "on_tick", err}) end
 end)
 
 
-game.on_event(defines.events.on_gui_click, function(event)
+script.on_event(defines.events.on_gui_click, function(event)
     local _, err = pcall(resmon.on_gui_click, event)
     if err then msg_all({"YARM-err-specific", "on_gui_click", err}) end
 end)
