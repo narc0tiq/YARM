c3a9102a11e65b67a04a61198230c62c089341bd
Martok88 <17170367+Martok88@users.noreply.github.com>
2017-10-16 12:52:23 -0700

    Add settings.lua

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "YARM-warn-percent",
+		setting_type = "runtime-per-user",
+		order = "a",
+		default_value = 10,
+		minimum_value = 1,
+		maximum_value = 100
+	}
+})
