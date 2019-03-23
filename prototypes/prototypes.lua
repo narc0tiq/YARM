e1f8718226da17c7c62d7f9753edca08e0d15aae
Octav Sandulescu <github@narc.ro>
2019-03-23 13:04:11 +0200

    Switch double to single quotes in data.lua
    Because it looks nicer

--- prototypes/prototypes.lua
+++ prototypes/prototypes.lua
@@ -8,56 +8,56 @@ data:extend(
     },
 
     {
-        type = "shortcut",
-        name = "yarm-selector",
-        action = "create-blueprint-item",
-        item_to_create = "yarm-selector-tool",
+        type = 'shortcut',
+        name = 'yarm-selector',
+        action = 'create-blueprint-item',
+        item_to_create = 'yarm-selector-tool',
         icon = {
-            filename = "__{{MOD_NAME}}__/graphics/resource-monitor.png",
-            priority = "extra-high-no-scale",
+            filename = '__{{MOD_NAME}}__/graphics/resource-monitor.png',
+            priority = 'extra-high-no-scale',
             size = 32,
             scale = 1,
-            flags = {"icon"},
+            flags = {'icon'},
         },
     },
 
     {
-        type = "selection-tool",
-        name = "yarm-selector-tool",
-        icon = "__{{MOD_NAME}}__/graphics/resource-monitor.png",
+        type = 'selection-tool',
+        name = 'yarm-selector-tool',
+        icon = '__{{MOD_NAME}}__/graphics/resource-monitor.png',
         icon_size = 32,
-        flags = {"only-in-cursor"},
+        flags = {'only-in-cursor'},
         stack_size = 1,
         stackable = false,
         selection_color = { g = 1 },
-        selection_mode = "any-entity",
+        selection_mode = 'any-entity',
         alt_selection_color = { g = 1, b = 1 },
-        alt_selection_mode = {"nothing"},
-        selection_cursor_box_type = "copy",
-        alt_selection_cursor_box_type = "copy",
-        entity_filter_mode = "whitelist",
-        entity_type_filters = {"resource"},
+        alt_selection_mode = {'nothing'},
+        selection_cursor_box_type = 'copy',
+        alt_selection_cursor_box_type = 'copy',
+        entity_filter_mode = 'whitelist',
+        entity_type_filters = {'resource'},
     },
 
     {
-        type = "container",
-        name = "rm_overlay",
-        flags = {"placeable-neutral", "player-creation", "not-repairable"},
-        icon = "__{{MOD_NAME}}__/graphics/rm_Overlay.png",
+        type = 'container',
+        name = 'rm_overlay',
+        flags = {'placeable-neutral', 'player-creation', 'not-repairable'},
+        icon = '__{{MOD_NAME}}__/graphics/rm_Overlay.png',
         icon_size = 32,
 
         max_health = 1,
         order = 'z[resource-monitor]',
 
-        collision_mask = {"resource-layer"},
+        collision_mask = {'resource-layer'},
         collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
 
         selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
         inventory_size = 1,
         picture =
         {
-            filename = "__{{MOD_NAME}}__/graphics/rm_Overlay.png",
-            priority = "extra-high",
+            filename = '__{{MOD_NAME}}__/graphics/rm_Overlay.png',
+            priority = 'extra-high',
             width = 32,
             height = 32,
             shift = {0.0, 0.0}
@@ -65,19 +65,19 @@ data:extend(
     },
 })
 
-local default_gui = data.raw["gui-style"].default
+local default_gui = data.raw['gui-style'].default
 
 local red_label = {
-    type = "label_style",
-    parent = "label",
+    type = 'label_style',
+    parent = 'label',
     font_color = {r=1, g=0.2, b=0.2}
 }
 default_gui.YARM_err_label = red_label
 
 local function button_graphics(xpos, ypos)
     return {
-        filename = "__{{MOD_NAME}}__/graphics/gui.png",
-        priority = "extra-high-no-scale",
+        filename = '__{{MOD_NAME}}__/graphics/gui.png',
+        priority = 'extra-high-no-scale',
         width = 16,
         height = 16,
         x = xpos,
@@ -86,14 +86,14 @@ local function button_graphics(xpos, ypos)
 end
 
 default_gui.YARM_outer_frame_no_border = {
-    type = "frame_style",
-    parent = "outer_frame",
+    type = 'frame_style',
+    parent = 'outer_frame',
     graphical_set = {}
 }
 
 default_gui.YARM_button_with_icon = {
-    type = "button_style",
-    parent = "slot_button",
+    type = 'button_style',
+    parent = 'slot_button',
 
     scalable = true,
 
@@ -112,17 +112,17 @@ default_gui.YARM_button_with_icon = {
 
 local function make_filter_buttons(base_name, tex_y)
     default_gui[base_name] = {
-        type = "button_style",
-        parent = "YARM_button_with_icon",
+        type = 'button_style',
+        parent = 'YARM_button_with_icon',
 
         default_graphical_set = button_graphics( 0, tex_y),
         hovered_graphical_set = button_graphics(16, tex_y),
         clicked_graphical_set = button_graphics(32, tex_y),
     }
 
-    default_gui[base_name.."_on"] = {
-        type = "button_style",
-        parent = "YARM_button_with_icon",
+    default_gui[base_name..'_on'] = {
+        type = 'button_style',
+        parent = 'YARM_button_with_icon',
 
         default_graphical_set = button_graphics(16, tex_y),
         hovered_graphical_set = button_graphics( 0, tex_y),
@@ -130,13 +130,13 @@ local function make_filter_buttons(base_name, tex_y)
     }
 end
 
-make_filter_buttons("YARM_filter_none", 48)
-make_filter_buttons("YARM_filter_warnings", 16)
-make_filter_buttons("YARM_filter_all", 32)
+make_filter_buttons('YARM_filter_none', 48)
+make_filter_buttons('YARM_filter_warnings', 16)
+make_filter_buttons('YARM_filter_all', 32)
 
 default_gui.YARM_overlay_site = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 64),
     hovered_graphical_set = button_graphics(16, 64),
@@ -144,8 +144,8 @@ default_gui.YARM_overlay_site = {
 }
 
 default_gui.YARM_goto_site = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 80),
     hovered_graphical_set = button_graphics(16, 80),
@@ -153,8 +153,8 @@ default_gui.YARM_goto_site = {
 }
 
 default_gui.YARM_delete_site = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 96),
     hovered_graphical_set = button_graphics(16, 96),
@@ -162,8 +162,8 @@ default_gui.YARM_delete_site = {
 }
 
 default_gui.YARM_rename_site = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 112),
     hovered_graphical_set = button_graphics(16, 112),
@@ -171,8 +171,8 @@ default_gui.YARM_rename_site = {
 }
 
 default_gui.YARM_delete_site_confirm = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 128),
     hovered_graphical_set = button_graphics(16, 128),
@@ -180,8 +180,8 @@ default_gui.YARM_delete_site_confirm = {
 }
 
 default_gui.YARM_goto_site_cancel = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 144),
     hovered_graphical_set = button_graphics(16, 144),
@@ -189,8 +189,8 @@ default_gui.YARM_goto_site_cancel = {
 }
 
 default_gui.YARM_rename_site_cancel = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 160),
     hovered_graphical_set = button_graphics(16, 160),
@@ -198,8 +198,8 @@ default_gui.YARM_rename_site_cancel = {
 }
 
 default_gui.YARM_expand_site = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 176),
     hovered_graphical_set = button_graphics(16, 176),
@@ -207,8 +207,8 @@ default_gui.YARM_expand_site = {
 }
 
 default_gui.YARM_expand_site_cancel = {
-    type = "button_style",
-    parent = "YARM_button_with_icon",
+    type = 'button_style',
+    parent = 'YARM_button_with_icon',
 
     default_graphical_set = button_graphics( 0, 192),
     hovered_graphical_set = button_graphics(16, 192),
@@ -216,23 +216,23 @@ default_gui.YARM_expand_site_cancel = {
 }
 
 default_gui.YARM_site_table = {
-    type = "table_style",
+    type = 'table_style',
     horizontal_spacing = 3,
     vertical_spacing = 1,
 }
 
 
 default_gui.YARM_buttons_h = {
-    type = "horizontal_flow_style",
-    parent = "horizontal_flow",
+    type = 'horizontal_flow_style',
+    parent = 'horizontal_flow',
     horizontal_spacing = 1,
     vertical_spacing = 5,
     top_padding = 4,
 }
 
 default_gui.YARM_buttons_v = {
-    type = "vertical_flow_style",
-    parent = "vertical_flow",
+    type = 'vertical_flow_style',
+    parent = 'vertical_flow',
     horizontal_spacing = 1,
     vertical_spacing = 5,
     top_padding = 4,
