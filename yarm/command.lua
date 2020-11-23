fa85944199c1853ccef8288cd6bcfc64c503d23a
Octav Sandulescu <github@narc.ro>
2020-11-23 22:53:24 +0100

    Add CLI response pagination where appropriate
    Console doesn't need it, since it just replies straight to log.

--- yarm/command.lua
+++ yarm/command.lua
@@ -26,6 +26,40 @@ function P.respond(e, msg)
     log(msg)
 end
 
+local LINES_PER_PAGE = 10
+
+function P.respond_paginated(e, lines)
+    if not e.player_index then
+        log(table.concat(lines, "\r\n"))
+        return
+    end
+
+    local page_num = tonumber(e.argv[#e.argv])
+    if page_num == nil then
+        page_num = 1
+        table.insert(e.argv, "1")
+    end
+    page_num = math.floor(page_num)
+    local page_count = math.max(1, math.ceil(#lines / LINES_PER_PAGE))
+    local command = table.concat(e.argv, ' ', 1, #e.argv - 1)
+    local warning = nil
+    if page_num < 1 or page_num > page_count then
+        warning = string.format("WARNING: Invalid page %s requested! There are %d pages.", page_num, page_count)
+        page_num = 1
+    end
+    
+
+    local first_line = (page_num - 1) * LINES_PER_PAGE + 1
+    local last_line = math.min(first_line + LINES_PER_PAGE, #lines)
+
+    local output = {
+        table.concat(lines, "\r\n", first_line, last_line),
+        string.format('Page %d of %d. Use "/yarm %s N" to view page N.', page_num, page_count, command),
+        warning
+    }
+    P.respond(e, table.concat(output, "\r\n"))
+end
+
 function P.commands.reinit()
     yarm.on_init()
 end
@@ -42,32 +76,40 @@ function P.commands.show(e)
 end
 
 function P.commands.show_monitors(e)
-    local result = {}
+    if #yarm.monitor.monitors == 0 then
+        P.respond(e, 'There are no monitors yet! Try placing one!')
+        return
+    end
+
+    local lines = {}
     for i, mondata in pairs(yarm.monitor.monitors) do
         local monitor_line = {i, ': at '}
         table.insert(monitor_line, util.positiontostr(mondata.position))
         table.insert(monitor_line, ', in site "')
         table.insert(monitor_line, mondata.site_name)
-        table.insert(monitor_line, '", sees:\r\n')
+        table.insert(monitor_line, '", sees:')
+        table.insert(lines, table.concat(monitor_line))
         for name, prod in pairs(mondata.product_types) do
-            table.insert(monitor_line, '\t- ')
-            table.insert(monitor_line, name)
-            table.insert(monitor_line, ': ')
-            table.insert(monitor_line, util.format_number(prod.amount))
-            table.insert(monitor_line, '/')
-            table.insert(monitor_line, util.format_number(prod.initial_amount))
-            table.insert(monitor_line, string.format(' (%.2f%%)', prod.amount * 100 / prod.initial_amount))
-            table.insert(monitor_line, string.format(' delta %s', util.format_number(prod.delta_per_minute)))
-            table.insert(monitor_line, ' ETD ')
-            table.insert(monitor_line, tostring(prod.minutes_to_deplete))
-            table.insert(monitor_line, ' minutes\r\n')
+            local product_line = { ' - ', name, ': '}
+            table.insert(product_line, util.format_number(prod.amount))
+            table.insert(product_line, '/')
+            table.insert(product_line, util.format_number(prod.initial_amount))
+            table.insert(product_line, string.format(' (%.2f%%)', prod.amount * 100 / prod.initial_amount))
+            table.insert(product_line, string.format(' delta %s', util.format_number(prod.delta_per_minute)))
+            table.insert(product_line, ' ETD ')
+            table.insert(product_line, tostring(prod.minutes_to_deplete))
+            table.insert(product_line, ' minutes')
+            table.insert(lines, table.concat(product_line))
         end
-        table.insert(result, table.concat(monitor_line))
     end
-    P.respond(e, table.concat(result))
+    P.respond_paginated(e, lines)
 end
 
 function P.commands.show_sites(e)
+    if table_size(yarm.site.sites) == 0 then
+        P.respond(e, 'There are no sites yet! Try placing a monitor.')
+        return
+    end
     P.respond(e, 'Not yet implemented, sorry')
 end
 
