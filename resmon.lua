d8e38c89bfa6bf81ca6186f4a0a498c44652a5dc
Christian Walde <walde.christian@gmail.com>
2023-05-09 16:08:43 +0200

    sketch of how to apply a background color to the overview

--- resmon.lua
+++ resmon.lua
@@ -845,6 +845,8 @@ function resmon.update_ui(player)
             tooltip={"YARM-tooltips.filter-warnings"}}
         buttons.add{type="button", name="YARM_filter_"..FILTER_ALL, style="YARM_filter_all",
             tooltip={"YARM-tooltips.filter-all"}}
+        buttons.add{type="button", name="YARM_toggle_bg", style="YARM_toggle_bg",
+            tooltip={"YARM-tooltips.toggle-bg"}}
 
         if not player_data.active_filter then player_data.active_filter = FILTER_WARNINGS end
         resmon.update_ui_filter_buttons(player, player_data.active_filter)
@@ -1205,6 +1207,14 @@ function resmon.on_click.expand_site(event)
     end
 end
 
+function resmon.on_click.toggle_bg(event)
+    local player = game.players[event.player_index]
+    local root = mod_gui.get_frame_flow(player).YARM_root
+    if not root then return end
+    root.style = (root.style.name == "YARM_outer_frame_no_border_bg")
+      and "YARM_outer_frame_no_border" or "YARM_outer_frame_no_border_bg"
+    resmon.update_ui(player)
+end
 
 function resmon.pull_YARM_item_to_cursor_if_possible(player_index)
     local player = game.players[player_index]
@@ -1320,6 +1330,8 @@ function resmon.on_gui_click(event)
         resmon.on_click.goto_site(event)
     elseif string.starts_with(event.element.name, "YARM_expand_site_") then
         resmon.on_click.expand_site(event)
+    elseif string.starts_with(event.element.name, "YARM_toggle_bg") then
+        resmon.on_click.toggle_bg(event)
     end
 end
 
