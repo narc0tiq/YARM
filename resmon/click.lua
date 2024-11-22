bd7b90eefa8091e3617fa2b8851a13059f9f910a
Octav Sandulescu <github@narc.ro>
2024-11-22 18:27:34 +0200

    Pass site name through tags rather than button name

--- resmon/click.lua
+++ resmon/click.lua
@@ -37,8 +37,7 @@ end
 
 ---@param event EventData.on_gui_click
 function click_module.delete_site(event)
-    local site_name = string.sub(event.element.name, 1 + string.len("YARM_delete_site_"))
-
+    local site_name = event.element.tags.site
     local player = game.players[event.player_index]
     local force_data = storage.force_data[player.force.name]
     local site = force_data.ore_sites[site_name]
@@ -58,8 +57,7 @@ end
 
 ---@param event EventData.on_gui_click
 function click_module.rename_site(event)
-    local site_name = string.sub(event.element.name, 1 + string.len("YARM_rename_site_"))
-
+    local site_name = event.element.tags.site
     local player = game.players[event.player_index]
     local player_data = storage.player_data[player.index]
 
@@ -85,8 +83,7 @@ end
 
 ---@param event EventData.on_gui_click
 function click_module.goto_site(event)
-    local site_name = string.sub(event.element.name, 1 + string.len("YARM_goto_site_"))
-
+    local site_name = event.element.tags.site
     local player = game.players[event.player_index]
     local force_data = storage.force_data[player.force.name]
     local site = force_data.ore_sites[site_name]
@@ -99,8 +96,7 @@ end
 ---One button handler for both the expand_site and expand_site_cancel buttons
 ---@param event EventData.on_gui_click
 function click_module.expand_site(event)
-    local site_name = string.sub(event.element.name, 1 + string.len("YARM_expand_site_"))
-
+    local site_name = event.element.tags.site
     local player = game.players[event.player_index]
     local player_data = storage.player_data[player.index]
     local force_data = storage.force_data[player.force.name]
